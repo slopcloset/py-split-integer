36f764f99a93bb8d76b13c742c2bd9b974d53665
slop <slopcloset@gmail.com>
2025-08-25 20:00:20 -0400

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,7 +4,7 @@ from app.split_integer import split_integer
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
     assert len(split_integer(17, 4)) == 4
-    
+
     assert sum(split_integer(32, 6)) == 32
     assert len(split_integer(32, 6)) == 6
 
@@ -16,7 +16,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert all(isinstance(x, int) for x in split_integer(6, 2))
     assert split_integer(6, 2) == sorted(split_integer(6, 2))
     assert max(split_integer(6, 2)) - min(split_integer(6, 2)) <= 1
-    
+
     assert split_integer(8, 4) == [2, 2, 2, 2]
     assert sum(split_integer(8, 4)) == 8
     assert len(split_integer(8, 4)) == 4
@@ -31,7 +31,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert len(split_integer(8, 1)) == 1
     assert all(isinstance(x, int) for x in split_integer(8, 1))
     assert split_integer(8, 1) == sorted(split_integer(8, 1))
-    
+
     assert split_integer(1, 1) == [1]
     assert sum(split_integer(1, 1)) == 1
     assert len(split_integer(1, 1)) == 1
@@ -46,20 +46,20 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     assert all(isinstance(x, int) for x in split_integer(17, 4))
     assert split_integer(17, 4) == sorted(split_integer(17, 4))
     assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
-    
+
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
     assert sum(split_integer(32, 6)) == 32
     assert len(split_integer(32, 6)) == 6
     assert all(isinstance(x, int) for x in split_integer(32, 6))
     assert split_integer(32, 6) == sorted(split_integer(32, 6))
     assert max(split_integer(32, 6)) - min(split_integer(32, 6)) <= 1
-    
+
     assert sum(split_integer(10, 3)) == 10
     assert len(split_integer(10, 3)) == 3
     assert all(isinstance(x, int) for x in split_integer(10, 3))
     assert split_integer(10, 3) == sorted(split_integer(10, 3))
     assert max(split_integer(10, 3)) - min(split_integer(10, 3)) <= 1
-    
+
     assert sum(split_integer(11, 4)) == 11
     assert len(split_integer(11, 4)) == 4
     assert all(isinstance(x, int) for x in split_integer(11, 4))
@@ -73,26 +73,26 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert all(isinstance(x, int) for x in split_integer(1, 2))
     assert split_integer(1, 2) == sorted(split_integer(1, 2))
     assert max(split_integer(1, 2)) - min(split_integer(1, 2)) <= 1
-    
+
     assert sum(split_integer(1, 3)) == 1
     assert len(split_integer(1, 3)) == 3
     assert all(isinstance(x, int) for x in split_integer(1, 3))
     assert split_integer(1, 3) == sorted(split_integer(1, 3))
     assert max(split_integer(1, 3)) - min(split_integer(1, 3)) <= 1
-    
+
     assert split_integer(5, 5) == [1, 1, 1, 1, 1]
     assert sum(split_integer(5, 5)) == 5
     assert len(split_integer(5, 5)) == 5
     assert all(isinstance(x, int) for x in split_integer(5, 5))
     assert split_integer(5, 5) == sorted(split_integer(5, 5))
     assert max(split_integer(5, 5)) - min(split_integer(5, 5)) <= 1
-    
+
     assert sum(split_integer(3, 5)) == 3
     assert len(split_integer(3, 5)) == 5
     assert all(isinstance(x, int) for x in split_integer(3, 5))
     assert split_integer(3, 5) == sorted(split_integer(3, 5))
     assert max(split_integer(3, 5)) - min(split_integer(3, 5)) <= 1
-    
+
     assert sum(split_integer(2, 7)) == 2
     assert len(split_integer(2, 7)) == 7
     assert all(isinstance(x, int) for x in split_integer(2, 7))
